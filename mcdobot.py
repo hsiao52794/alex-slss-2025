b0008f66a0270a4f6ada9edcc25eec5cfceb1657
Alex Hsiao - 293 <hsiao52794@gmail.com>
2025-10-07 11:15:14 -0700

    add

--- mcdobot.py
+++ mcdobot.py
@@ -2,6 +2,15 @@
 # Author: Alex
 # Oct 6 2025
 
+"""
+Write a McDonald's bot that asks if you want fries with your meal.
+Call it work-mcdobot.py .
+It should accept Yes/yes or No/no as inputs, and reply
+appropriately depending on the answer.
+If the user inputs anything else, it should repeat back their answer
+and say that it does not understand.
+"""
+
 print("Hi")
 print("What would you like for today?")
 meal = str(input(": "))
