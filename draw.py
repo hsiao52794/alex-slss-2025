0c6d38eef7e0fac935d068ec3903aed0de296a5a
Alex Hsiao - 293 <hsiao52794@gmail.com>
2025-10-31 12:53:13 -0700

    save

--- draw.py
+++ draw.py
@@ -63,7 +63,6 @@ if select == str(1):
     # start
     pen3.penup()
     pen3.right(-45)
-    import math
     number1 = math.sqrt(2*(size**2))
     pen3.forward(2*number1)
     pen3.left(85)
@@ -215,40 +214,57 @@ if select == str(4):
     draw_fibonacci()
 
 if select == str(5):
-    size = int(input("size(suggested: 10): "))
+    size = int(input("size: "))
     repeat = int(input("repeat: "))
-    pen1 = turtle.Turtle()
+    sides = int(input("sides(sides>=3): "))
+
+    def all_data():
+        print(f"ang1: {ang1}")
+        print(f"ang2: {ang2}")
+        print(f"ang3: {ang3}")
+        print(f"L: {L}")
+        print(f"L1: {L1}")
+        print(f"L2: {L2}")
+        print(f"r: {r}")
+        print(f"counter: {counter}")
+
     pen1.color("black")
     pen1.shape("arrow")
     pen1.shapesize(1)
     pen1.speed(20)
 
-    ang1 = 0
+    repeat = int((repeat*(360/sides)-1))
+    ang1 = 1
 
     pen1.pu()
     L = size
+    n = 1
     for counter in range(repeat):
-        ang2 = 180-(90+ang1)
-        L1 = math.sin(math.radians(ang2))*(L/math.sin(math.radians(90)))
-        L2 = math.sin(math.radians(ang1))*(L/math.sin(math.radians(90)))
-        pen1.forward(L/2)
-        pen1.right(-90)
-        pen1.forward(L/2)
-        pen1.right(180)
+        if ang1 >= 360/sides:
+            ang1 = 1
+            n = n + 1
+        ang2 = 180-(360/sides)
+        ang3 = 180-(ang2+ang1)
+        import math
+        L1 = math.sin(math.radians(ang3))*(L/math.sin(math.radians(ang2)))
+        L2 = math.sin(math.radians(ang1))*(L/math.sin(math.radians(ang2)))
+        r = (L/2)/math.cos(math.radians(ang2/2))
+        if (L1 + L2) < L:
+            all_data()
+            continue
+        pen1.forward(r)
+        pen1.right(180-(ang2/2))
         pen1.pd()
-        for color in ["red", "orange", "green", "blue"]:
-            pen1.color(color)
+        for counter in range(sides):
             pen1.forward(L)
-            pen1.right(90)
+            pen1.right(180-ang2)
         pen1.pu()
-        pen1.forward(L/2)
-        pen1.right(90)
-        pen1.forward(L/2)
+        pen1.right(ang2/2)
+        pen1.forward(r)
         pen1.right(180)
         pen1.right(ang1)
         L = L1 + L2
         ang1 = ang1 + 1
-    pen1.shapesize(0.01)
 
 turtle.exitonclick()
 turtle.done
