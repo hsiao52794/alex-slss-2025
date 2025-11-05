7441d8df551435c8ab50116776f6c843fb630128
Alex Hsiao - 293 <hsiao52794@gmail.com>
2025-11-05 13:05:43 -0800

    svae

---
+++ notes-6-numbers.py
@@ -0,0 +1,17 @@
+# create an algorithm to gather
+# data to find the most popular
+# bubble tea place around us
+
+# v1
+# show choices
+# ask their choice
+# add their vote to a running tally
+# give some raw scores
+# percentage
+
+# v2
+# ask user fave
+# add their vote to a running
+# tally
+# give the raw score
+# give the score as percentage
