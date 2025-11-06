ba9996a34f75f9bbd1a76a5aa20286b36627278d
Alex Hsiao - 293 <hsiao52794@gmail.com>
2025-11-06 13:02:17 -0800

    svae

--- notes-6-numbers.py
+++ notes-6-numbers.py
@@ -3,11 +3,26 @@
 # bubble tea place around us
 
 # v1
-# show choices
-# ask their choice
-# add their vote to a running tally
-# give some raw scores
-# percentage
+def vote_listed_choices():
+    """display all voting choicess
+    5 user vote for their choice
+    results will be printed"""
+    CHOICES = [
+        "A. CoCo",
+        "B. Chatime",
+        "C. BUBBLE WAFFEL",
+        "D. gong cha"
+    ]
+    # show choices
+    print("vote for your favourite from the list.")
+    print("give the letter of your choice.")
+    for choice in CHOICES:
+        print(choice)
+    # ask their choice
+    # add their vote to a running
+    # tally
+    # give some raw scores
+    # percentage
 
 # v2
 # ask user fave
@@ -15,3 +30,9 @@
 # tally
 # give the raw score
 # give the score as percentage
+
+def main():
+    vote_listed_choices()
+
+if __name__ == "__main__":
+    main()
