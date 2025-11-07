f32880167336dcfef4d5125fdfb9bd4d5339c8eb
Alex Hsiao - 293 <hsiao52794@gmail.com>
2025-11-07 11:19:05 -0800

    svae

--- notes-6-numbers.py
+++ notes-6-numbers.py
@@ -2,6 +2,9 @@
 # data to find the most popular
 # bubble tea place around us
 
+import os
+NUM_VOTERS = 10
+
 # v1
 def vote_listed_choices():
     """display all voting choicess
@@ -18,26 +21,44 @@ def vote_listed_choices():
     chatime = 0
     bubble_waffel = 0
     gong_cha = 0
-    # show choices
-    print("Vote for your favourite from the list.")
-    print("Give the letter of your choice.")
-    for choice in CHOICES:
-        print(choice)
-    # ask their choice
-    vote = input("Your vote: ").lower().strip(",.? ")
+    spoiled_votes = 0
 
-    # add their vote to a running
-    # tally
-    if vote == "a":
-        coco = coco + 1
-    if vote == "b":
-        chatime += 1
-    if vote == "c":
-        bubble_waffel += 1
-    if vote == "d":
-        gong_cha += 1
-    # give some raw scores
-    # percentage
+    for _ in range(NUM_VOTERS):
+        os.system("clear")
+        print("Vote for your favourite from the list.")
+        print("Give the letter of your choice.")
+        for choice in CHOICES:
+            print(choice)
+        vote = input("Your vote: ").lower().strip(",.?! ")
+
+        # add their vote to a running
+        # tally
+        if vote == "a":
+            coco = coco + 1
+        elif vote == "b":
+            chatime += 1
+        elif vote == "c":
+            bubble_waffel += 1
+        elif vote == "d":
+            gong_cha += 1
+        else:
+            spoiled_votes += 1
+        # give some raw scores
+        # Show the score of CoCo
+        print(f"CoCo votes: {coco}")
+        print(f"Chatime votes: {chatime}")
+        print(f"BUBBLE WAFFEL votes: {bubble_waffel}")
+        print(f"Gong Cha votes: {gong_cha}")
+        print(f"Spoiled votes: {spoiled_votes}")
+        # percentage
+        coco_percentage = coco / (coco + chatime + bubble_waffel + gong_cha + spoiled_votes)
+        print(f"CoCo Percentage: {coco_percentage * 100}%")
+        chatime_percentage = chatime / (coco + chatime + bubble_waffel + gong_cha + spoiled_votes)
+        print(f"Chatime Percentage: {chatime_percentage * 100}%")
+        bubble_waffel_percentage = bubble_waffel / (coco + chatime + bubble_waffel + gong_cha + spoiled_votes)
+        print(f"Bubble Waffel Percentage: {bubble_waffel_percentage * 100}%")
+        gong_cha_percentage = gong_cha / (coco + chatime + bubble_waffel + gong_cha + spoiled_votes)
+        print(f"Gong Cha Percentage: {gong_cha_percentage * 100}%")
 
 # v2
 # ask user fave
