c39abc1d5fc232e81f3c765e9a933cb1b0b66776
hsiao52794 <hsiao52794@gmail.com>
2025-12-01 10:38:51 -0800

    Implement angle normalization in calc-trig.py
    Refactor angle calculations to use a separate normalization function.

--- calc-trig.py
+++ calc-trig.py
@@ -1,38 +1,25 @@
+import math
+
+def normalize_angle(angle: float) -> float:
+    if angle < 90:
+        angle = 180 - angle
+
+    return 360 - (angle - 25)
+
 print("Enter the distance from spot A to the target.")
 pos_A = int(input(": "))
 print("Enter the distance from spot B to the target.")
 pos_B = int(input(": "))
 
-import math
-
-a = pos_A
-b = pos_B
 c = 11
 
-ang_a = math.acos((b**2 + c**2 - a**2)/(2 * b * c))
+ang_a = math.acos((pos_B**2 + c**2 - pos_A**2)/(2 * pos_B * c))
 ang_a = math.degrees(ang_a)
-ang_b = math.acos((a**2 + c**2 - b**2)/(2 * a * c))
+ang_b = math.acos((pos_A**2 + c**2 - pos_B**2)/(2 * pos_A * c))
 ang_b = math.degrees(ang_b)
 
-"""
-i was going to do it like this, but it didnt work. help me if u can.
-
-for ang in [ang_a, ang_b]:
-    if ang < 90:
-        ang = 180 - ang
-
-    ang = 360 - (ang -25)
-"""
-
-if ang_a < 90:
-    ang_a = 180 - ang_a
-
-ang_a = 360 - (ang_a - 25)
-
-if ang_b < 90:
-    ang_b = 180 - ang_b
-
-ang_b = 360 - (ang_b - 25)
+ang_a = normalize_angle(ang_a)
+ang_b = normalize_angle(ang_b)
 
 ang = (ang_a + ang_b) / 2
 
